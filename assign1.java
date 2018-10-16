fbc6c6c0821442c7c3725a9c730a003691238e58
Jeremiah Smith <Jerry@iMac.local>
2018-10-16 19:34:35 +1100

    deleted some unnecessary code

--- assign1.java
+++ assign1.java
@@ -27,37 +27,37 @@ import org.w3c.dom.NodeList;
 
 
 class MyErrorHandler implements ErrorHandler { // private static
-     
-    private PrintWriter out;
 
-    MyErrorHandler(PrintWriter out) {
-        this.out = out;
-    }
+	private PrintWriter out;
 
-    private String getParseExceptionInfo(SAXParseException spe) {
-        String systemId = spe.getSystemId();
-        if (systemId == null) {
-            systemId = "null";
-        }
+	MyErrorHandler(PrintWriter out) {
+		this.out = out;
+	}
 
-        String info = "URI=" + systemId + " Line=" + spe.getLineNumber() +
-                      ": " + spe.getMessage();
-        return info;
-    }
+	private String getParseExceptionInfo(SAXParseException spe) {
+		String systemId = spe.getSystemId();
+		if (systemId == null) {
+			systemId = "null";
+		}
 
-    public void warning(SAXParseException spe) throws SAXException {
-        out.println("Warning: " + getParseExceptionInfo(spe));
-    }
-        
-    public void error(SAXParseException spe) throws SAXException {
-        String message = "Error: " + getParseExceptionInfo(spe);
-        throw new SAXException(message);
-    }
+		String info = "URI=" + systemId + " Line=" + spe.getLineNumber() +
+			": " + spe.getMessage();
+		return info;
+	}
 
-    public void fatalError(SAXParseException spe) throws SAXException {
-        String message = "Fatal Error: " + getParseExceptionInfo(spe);
-        throw new SAXException(message);
-    }
+	public void warning(SAXParseException spe) throws SAXException {
+		out.println("Warning: " + getParseExceptionInfo(spe));
+	}
+
+	public void error(SAXParseException spe) throws SAXException {
+		String message = "Error: " + getParseExceptionInfo(spe);
+		throw new SAXException(message);
+	}
+
+	public void fatalError(SAXParseException spe) throws SAXException {
+		String message = "Fatal Error: " + getParseExceptionInfo(spe);
+		throw new SAXException(message);
+	}
 }
 
 public class assign1
@@ -75,46 +75,33 @@ public class assign1
 		System.out.println("Total of elements : " + stationList.getLength());
 		int stations = stationList.getLength();
 
-		System.out.println(stations + " stations");
+		NodeList stationNodes = document.getElementsByTagName("Station");
+		List<Station> list = new ArrayList<Station>();
+		for (int i = 0; i < stationNodes.getLength(); i++)
+		{
+			NodeList stationAttributes = stationNodes.item(i).getChildNodes();
+			String Name = stationAttributes.item(1).getTextContent();
+			String Line = stationAttributes.item(3).getTextContent();
+			// System.out.println(stationAttributes.item(5).getNodeName());
+			NodeList stationEdges = stationAttributes.item(5).getChildNodes();
 
-		String name = document.getElementsByTagName("Name").item(0).getTextContent();
-		String line = document.getElementsByTagName("Line").item(0).getTextContent();
+			System.out.println(i);
+			list.add(new Station(Name, Line, stationEdges));
 
-		// System.out.println("last station is " + document.getElementsByTagName("Station").item(stations).getAttribute());//.getContent().getElementsByTagName("Name").getTextContent());
-		 System.out.println("Root element: " + document.getDocumentElement().getNodeName());
+		}
 
-		 NodeList stationNodes = document.getElementsByTagName("Station");
-		 List<Station> list = new ArrayList<Station>();
-		 for (int i = 0; i < stationNodes.getLength(); i++)
-		 {
-			 NodeList stationAttributes = stationNodes.item(i).getChildNodes();
-			 String Name = stationAttributes.item(1).getTextContent();
-			 String Line = stationAttributes.item(3).getTextContent();
-			 // System.out.println(stationAttributes.item(5).getNodeName());
-			 NodeList stationEdges = stationAttributes.item(5).getChildNodes();
-
-			 System.out.println(i);
-			 list.add(new Station(Name, Line, stationEdges));
-
-		 }
-
-		 // prints stored object data in order of XML file
-		 for (int i = 0; i < stationNodes.getLength(); i++)
-		 {
-			 System.out.println("Station Name: " +list.get(i).getName());
-			 System.out.println("Line: "+list.get(i).getLine());
-			 for (int a = 0; a < list.get(i).getEdges().size(); a++)
-			 {
-				 System.out.println("	Edge:");
-				 System.out.println("		Name: "+list.get(i).getEdges().get(a).getName());
-				 System.out.println("		Line: "+list.get(i).getEdges().get(a).getLine());
-				 System.out.println("		Duration: "+list.get(i).getEdges().get(a).getDuration());
-			 }
-		 }
-
-
-
-		// System.out.println(name);
-		// System.out.println(line);
+		// prints stored object data in order of XML file
+		for (int i = 0; i < stationNodes.getLength(); i++)
+		{
+			System.out.println("Station Name: " +list.get(i).getName());
+			System.out.println("Line: "+list.get(i).getLine());
+			for (int a = 0; a < list.get(i).getEdges().size(); a++)
+			{
+				System.out.println("	Edge:");
+				System.out.println("		Name: "+list.get(i).getEdges().get(a).getName());
+				System.out.println("		Line: "+list.get(i).getEdges().get(a).getLine());
+				System.out.println("		Duration: "+list.get(i).getEdges().get(a).getDuration());
+			}
+		}
 	}
 }
