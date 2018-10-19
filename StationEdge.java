a5c113e1a36bd55a58fae2a5dd72b45cb31d912a
juyong kim <Juyong.Kim@uon.edu.au>
2018-10-19 15:11:12 +1100

    added source and destination variables

--- StationEdge.java
+++ StationEdge.java
@@ -13,14 +13,21 @@ public class StationEdge
 	private String Name;
 	private String Line;
 	private int Duration;
+	//additions based off the vogella
+	private String Source;
+	private String Destination;
 
-	public StationEdge(String Name, String Line, int Duration)
+	//constructor
+	public StationEdge(String Name, String Line, int Duration, String Source, String Destination)
 	{
 		this.Name = Name;
 		this.Line = Line;
 		this.Duration = Duration;
+		this.Source = Source;
+		this.Destination = Destination;
 	}
 
+	//getters
 	public String getName()
 	{
 		return Name;
@@ -32,5 +39,21 @@ public class StationEdge
 	public int getDuration()
 	{
 		return Duration;
+	}
+	public String getSource()
+	{
+		return Source;
+	}
+	public String getDestination()
+	{
+		return Destination;
+	}
+
+	//side note, may or may not use
+	//used for printing 
+	@Override
+	public String toString()
+	{
+		return Name + " " + Destination;
 	}
 }
