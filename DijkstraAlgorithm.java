5f148e785d85a19fc8ded4797373a7cbdb2ad909
juyong kim <Juyong.Kim@uon.edu.au>
2018-10-19 17:02:36 +1100

    just making the functions

--- DijkstraAlgorithm.java
+++ DijkstraAlgorithm.java
@@ -23,11 +23,52 @@ public class DijkstraAlgorithm
         this.nodes = new ArrayList<Station>(graph.getVertices());
         this.edges = new ArrayList<StationEdge>(graph.getEdges());
     }
+
+    //goes through all the nodes in a bfs
     public void execute(Station source)
     {
         visitedNodes = new HashSet<Station>();
         notVisitedNodes = new HashSet<Station>();
         previousNode = new HashMap<Station, Station>();
         duration = new HashMap<Station, Integer>();
+
+        //creates the starting station 
+        duration.put(source, 0);
+        notVisitedNodes.add(source);
+
+        while(notVisitedNodes.size() > 0)
+        {
+            Station node = getMinimum(notVisitedNodes);
+            visitedNodes.add(node);
+            notVisitedNodes.remove(node);
+            findMinimalDistances(node);
+        }
     }
+    private void findMinimalDistances(Station station1)
+    {
+
+    }
+    private int getDuration(Station station1, Station station2)
+    {
+
+    }
+    private List<Station> getNeighbours(Station station1)
+    {
+
+    }
+    private Station getMinimum(Set<Station> stations)
+    {
+
+    } 
+    private boolean isVisited(Station station1)
+    {
+
+    }
+    private int getShortestDistance(Station station1)
+    {
+
+    }
+
+    //create path function for printing
+
 }
