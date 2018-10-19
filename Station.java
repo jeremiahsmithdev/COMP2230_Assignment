e61f6f6e7c48731f7a225024db22285150e310ac
juyong kim <Juyong.Kim@uon.edu.au>
2018-10-19 17:33:35 +1100

    merging for push

--- Station.java
+++ Station.java
@@ -9,8 +9,7 @@
 // TODO modify according to http://www.vogella.com/tutorials/JavaAlgorithmsDijkstra/article.html
 import java.util.ArrayList;
 import java.util.List;
-import org.w3c.dom.NodeList;
-
+import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 public class Station
@@ -48,7 +47,7 @@ public class Station
 
 		for(int i=0; i < stations.getLength(); i++)
 		{
-			if(stations.get(i).getName()==name &&stations.get(i).getLine()==line)
+			if(stations.get(i).getName()==name && stations.get(i).getLine()==line)
 			{
 				return stations.get(i);
 			}
