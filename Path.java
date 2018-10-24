58d3fd8bff49569a3659c090721664f565d76998
juyong kim <Juyong.Kim@uon.edu.au>
2018-10-25 10:43:31 +1100

    finished commenting Path.java

--- Path.java
+++ Path.java
@@ -1,30 +1,39 @@
+/**
+ * *
+ *  * Station.java – Assignment1
+ *   * @author: Jeremiah Smith, Juyong Kim
+ *    * @student Number: c3238179 c3244203
+ *     * @version: 016/10/2018
+ *      * Description: Holds information to create and store a Path
+ *       */
 public class Path
 {
+	//variables
 	private String path;
 	private int time;
 	private int changes;
 
+	//Empty Constructor
 	public Path()
 	{
 	}
 
-	public int getTime()
-	{
-		return time;
-	}
-
-	public int getChanges()
-	{
-		return changes;
-	}
-
+	//Setters
 	public void setTime(int time)
 	{
 		this.time = time;
 	}
-
 	public void setChanges(int changes)
 	{
 		this.changes = changes;
+	}
+	//Getters
+	public int getTime()
+	{
+		return time;
+	}
+	public int getChanges()
+	{
+		return changes;
 	}
 }
